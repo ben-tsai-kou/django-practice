95141090c2b2be211930346150647ef15b7ccf02
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-06-19 18:55:00 +0900

    customizing the admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Product, Offer
 
-admin.site.register(Product)
-admin.site.register(Offer)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+    search_fields = ('name',)
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Offer, OfferAdmin)
